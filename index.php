352a19e346642e83d281c988b1763d4f5fe0f52e
Nozdormus1 <nozdormus@mail.ru>
2016-01-22 23:33:49 +0200

    changed

--- index.php
+++ index.php
@@ -11,11 +11,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT * FROM main";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
          echo "Hello, " . $row["keyword"]. "<br> current build is build#"
     }
